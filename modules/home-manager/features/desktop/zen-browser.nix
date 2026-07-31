9c7262e4b6094007da0c88cc297b48f13861c7bc
Teamofeyy <timarnd06@gmail.com>
2026-07-31 14:50:19 +0300

    feat(zen): add essential pins

--- modules/home-manager/features/desktop/zen-browser.nix
+++ modules/home-manager/features/desktop/zen-browser.nix
@@ -38,14 +38,61 @@ in {
         "clipper@obsidian.md" = normalExtension "web-clipper-obsidian";
         "@testpilot-containers" = normalExtension "multi-account-containers";
         "uBlock0@raymondhill.net" = normalExtension "ublock-origin";
+        "@zen-auto-close-tabs" = normalExtension "zen-auto-close-tabs"; 
       };
     };
 
     profiles.${config.home.username} = {
       isDefault = true;
 
+      pins = {
+        "Gmail" = {
+          id = "795e8397-7a44-4086-95f4-af54d8b24665";
+          url = "https://mail.google.com/mail/u/0/#inbox";
+          position = 101;
+          isEssential = true;
+        };
+        "GitLab" = {
+          id = "5a1e0be2-2e3d-4202-b795-18e69d901e44";
+          url = "https://gitlab.internal.madrigal.ru/";
+          position = 102;
+          isEssential = true;
+        };
+        "Confluence" = {
+          id = "94bc7990-7a91-4fdd-ae2d-6df49221badd";
+          url = "https://confluence.internal.madrigal.ru/index.action#all-updates";
+          position = 103;
+          isEssential = true;
+        }; 
+        "Jira" = {
+          id = "a0013f20-47e1-474f-b512-b57c55ac8de1";
+          url = "https://jira.internal.madrigal.ru/secure/RapidBoard.jspa?rapidView=9&projectKey=MSS";
+          position = 104;
+          isEssential = true;
+        }; 
+        "ChatGPT" = {
+          id = "20f11868-990f-4f26-8dcc-2ad901e36c6c";
+          url = "https://chatgpt.com";
+          position = 105;
+          isEssential = true;
+        }; 
+        "Github" = {
+          id = "8d061451-64cd-41ea-bb59-44ada9537288";
+          url = "https://github.com/Teamofeyy?tab=repositories";
+          position = 106;
+          isEssential = true;
+        }; 
+        "DeepSeek" = {
+          id = "379d4290-176a-4d42-a5f6-3d4d030728f9";
+          url = "https://chat.deepseek.com";
+          position = 107;
+          isEssential = true;
+        }; 
+        
+      };
+
       mods = [
-        "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+        "72f8f48d-86b9-4487-acea-eb4977b18f21" #  Better Ctrl+Tab panel
         "253a3a74-0cc4-47b7-8b82-996a64f030d5" # Floating History
       ];
 
